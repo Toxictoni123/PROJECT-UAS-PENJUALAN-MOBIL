f308349428b70aa61a14b02fe9d8e8b27defbfa3
Toxictoni123 <58581607+Toxictoni123@users.noreply.github.com>
2020-06-14 19:37:07 +0700

    menambahkan file userServet

---
+++ src/java/model/user.java
@@ -0,0 +1,43 @@
+package model;
+
+public class user {
+    private String id_user;
+    private String nm_user;
+    private String hak_akses;
+    private String password;
+
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getNm_user() {
+        return nm_user;
+    }
+
+    public void setNm_user(String nm_user) {
+        this.nm_user = nm_user;
+    }
+
+    public String getHak_akses() {
+        return hak_akses;
+    }
+
+    public void setHak_akses(String hak_akses) {
+        this.hak_akses = hak_akses;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+
+}
