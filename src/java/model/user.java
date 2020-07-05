46422f9c7c40dae0eb3b995a82c1e3c19d563a99
Nmlapdnh <nurmallpradenah@gmail.com>
2020-07-05 18:58:15 +0700

    modified user.java

--- src/java/model/user.java
+++ src/java/model/user.java
@@ -1,10 +1,35 @@
 package model;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import control.koneksi;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class user {
+    private String id;
     private String id_user;
     private String nm_user;
     private String hak_akses;
     private String password;
+    koneksi db = null;
+
+    public user(){
+        try{
+            db = new koneksi();
+        } catch (ClassNotFoundException ex){
+            Logger.getLogger(user.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getId_user() {
         return id_user;
@@ -36,5 +61,28 @@ public class user {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    
+    public List LoginUser(String user, String password){
+        List data = new ArrayList();
+        ResultSet rs = null;
+        try{
+            String sql = "SELECT * FROM user WHERE id_user='" + user + "'"
+                    + " AND password='" + password + "'";
+            rs = db.ambilData(sql);
+            
+            while (rs.next()){
+                user am = new user();
+                am.setId(rs.getString("id"));
+                am.setId_user(rs.getString("id_user"));
+                am.setPassword(rs.getString("password"));
+                am.setHak_akses(rs.getString("hak_akses"));
+                data.add(am);
+            }
+            db.diskonek(rs);
+        } catch(Exception a){
+            System.out.println("Terjadi Kesalahan Cari Login User, pada :\n" + a);
+        }
+        return data;
     }
 }
